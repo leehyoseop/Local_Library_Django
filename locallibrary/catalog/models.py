046471dbc7b52a6ee7ac0212a842daecc57eb007
leehyoseop <0303hslee@naver.com>
2020-05-29 17:03:41 +0900

    PART2

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,13 +1,21 @@
 from django.db import models
-from django.urls import reverse
+
 # Create your models here.
+
+from django.urls import reverse  # To generate URLS by reversing URL patterns
+
+
 class Genre(models.Model):
-    """Model representing a book genre."""
-    name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
-    
+    """Model representing a book genre (e.g. Science Fiction, Non Fiction)."""
+    name = models.CharField(
+        max_length=200,
+        help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)"
+        )
+
     def __str__(self):
-        """String for representing the Model object."""
+        """String for representing the Model object (in Admin site etc.)"""
         return self.name
+
 
 class Language(models.Model):
     """Model representing a Language (e.g. English, French, Japanese, etc.)"""
@@ -18,55 +26,58 @@ class Language(models.Model):
         """String for representing the Model object (in Admin site etc.)"""
         return self.name
 
+
 class Book(models.Model):
     """Model representing a book (but not a specific copy of a book)."""
-    title = models.CharField(max_length=200)
-
+    title = models.CharField(max_length=200, verbose_name = '제목')
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True, verbose_name = '저자')
     # Foreign Key used because book can only have one author, but authors can have multiple books
-    # Author as a string rather than object because it hasn't been declared yet in the file
-    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-    
-    summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
-    isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
-    
-    # ManyToManyField used because genre can contain many books. Books can cover many genres.
+    # Author as a string rather than object because it hasn't been declared yet in file.
+    summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book", verbose_name = '요약')
+    isbn = models.CharField('ISBN', max_length=13,
+                            help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn'
+                                      '">ISBN number</a>')
+    genre = models.ManyToManyField(Genre, help_text="Select a genre for this book", verbose_name = '장르')
+    # ManyToManyField used because a genre can contain many books and a Book can cover many genres.
     # Genre class has already been defined so we can specify the object above.
-    genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
-    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
-    '''
+    language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True, verbose_name = '언어')
+
     def display_genre(self):
         """Creates a string for the Genre. This is required to display genre in Admin."""
         return ', '.join([genre.name for genre in self.genre.all()[:3]])
 
     display_genre.short_description = 'Genre'
-    '''
+
+    def get_absolute_url(self):
+        """Returns the url to access a particular book instance."""
+        return reverse('book-detail', args=[str(self.id)])
+
     def __str__(self):
         """String for representing the Model object."""
         return self.title
-    
-    def get_absolute_url(self):
-        """Returns the url to access a detail record for this book."""
-        return reverse('book-detail', args=[str(self.id)])
 
-import uuid # Required for unique book instances
+
+import uuid  # Required for unique book instances
 from datetime import date
+
 from django.contrib.auth.models import User  # Required to assign User as a borrower
+
 
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
-    book = models.ForeignKey(Book, on_delete=models.SET_NULL, null=True) 
-    #book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text="Unique ID for this particular book across whole library")
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
     borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-    
+
     @property
     def is_overdue(self):
         if self.due_back and date.today() > self.due_back:
             return True
         return False
-    
+
     LOAN_STATUS = (
         ('m', 'Maintenance'),
         ('o', 'On loan'),
@@ -79,16 +90,16 @@ class BookInstance(models.Model):
         choices=LOAN_STATUS,
         blank=True,
         default='m',
-        help_text='Book availability',
-    )
+        help_text='Book availability')
 
     class Meta:
         ordering = ['due_back']
-        #permissions = (("can_mark_returned", "Set book as returned"),)
+        permissions = (("can_mark_returned", "Set book as returned"),)
 
     def __str__(self):
         """String for representing the Model object."""
-        return f'{self.id} ({self.book.title})'
+        return '{0} ({1})'.format(self.id, self.book.title)
+
 
 class Author(models.Model):
     """Model representing an author."""
